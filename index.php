82930f15f133d5962319bb3ddff1d05f5ffad2cd
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-04-17 23:39:06 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <title>php</title>
+</head>
+<body>
+    <?php
+        echo'こんにちは';
+        echo'PHPを学ぼう';
+    ?>
+</body>
+</html>
